04de4643a3205dd6a6e46bc6983ac7b0241f560a
FloSports Engineering <engineering@flosports.tv>
2020-02-19 16:07:21 +0000

    CircleCI auto commit

--- js/dist/modal.js
+++ js/dist/modal.js
@@ -1,5 +1,5 @@
 /*!
-  * Bootstrap modal.js v0.0.8 (https://flo-scss.flo.center/)
+  * Bootstrap modal.js v0.0.10 (https://flo-scss.flo.center/)
   * Copyright 2011-2020 FloSports Engineering (https://flosports.tv) & The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
